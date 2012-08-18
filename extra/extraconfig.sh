a9bf75b4be8121f046c3f97958fd688886b74c11
Gregor Richards <Richards@codu.org>
2012-08-18 06:36:02 -0400

    MIPS no longer requires any extra configuration.

--- extra/extraconfig.sh
+++ extra/extraconfig.sh
@@ -5,11 +5,6 @@ case "$ARCH" in
         GCC_BOOTSTRAP_CONFFLAGS="$GCC_BOOTSTRAP_CONFFLAGS --with-arch=armv4t"
         GCC_CONFFLAGS="$GCC_CONFFLAGS --with-arch=armv4t"
         ;;
-
-    mips*)
-        GCC_BOOTSTRAP_CONFFLAGS="$GCC_BOOTSTRAP_CONFFLAGS --with-arch=mips2"
-        GCC_CONFFLAGS="$GCC_CONFFLAGS --with-arch=mips2"
-        ;;
 esac
 
 case "$ARCH" in
