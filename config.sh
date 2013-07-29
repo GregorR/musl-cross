971efe6f90fa032e7d146d16d5f9f978ef07f2eb
rofl0r <retnyg@gmx.net>
2013-07-29 06:40:07 +0200

    add working config

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
 # ARCH will be auto-detected as the host if not specified
-#ARCH=i486
-CC_BASE_PREFIX=/opt/cross
+ARCH=arm
+CC_BASE_PREFIX=/home/rofl/musl-cross-prefix
 MAKEFLAGS=-j8
+GCC_VERSION=4.7.3
+MUSL_GIT_VERSION=7efa12a04ac4a14d243c2276519e152ed815b4d2
+MUSL_GIT=yes
+MUSL_VERSION=7efa12a04ac4a14d243c2276519e152ed815b4d2
+GCC_BUILTIN_PREREQS=yes
