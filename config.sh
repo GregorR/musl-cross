e20d60b044d465968ea3a35000ee1d2e2f213083
rofl0r <retnyg@gmx.net>
2013-12-10 17:27:34 +0100

    config.sh: accept pseudo-archs armv7[hf]
    and automatically fill in the right values for them.
    the ARCH is reset to "arm".

--- config.sh
+++ config.sh
@@ -6,18 +6,24 @@
 #ARCH=microblaze
 #ARCH=mips
 #ARCH=mipsel
+#ARCH=armv7hf
+#ARCH=armv7
 
 CC_BASE_PREFIX=/opt/cross
-
-# if you use arm, you may need more fine-tuning:
-# arm hardfloat v7
-#TRIPLE=arm-linux-musleabihf
-#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
-#GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
-
-#arm softfp
-#TRIPLE=arm-linux-musleabi
-#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
-#GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
-
 MAKEFLAGS=-j8
+
+if [ "$ARCH" = armv7hf ] ; then
+	# arm hardfloat v7
+	TRIPLE=arm-linux-musleabihf
+	GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+	GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+	ARCH=arm
+
+elif [ "$ARCH" = armv7 ] ; then
+	#arm softfp
+	TRIPLE=arm-linux-musleabi
+	GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
+	GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
+	ARCH=arm
+fi
+
