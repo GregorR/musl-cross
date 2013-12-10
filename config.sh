1abc37870ee22015155c26da9916d2ee55c19781
rofl0r <retnyg@gmx.net>
2013-12-10 14:36:51 +0100

    config.sh: add selection of values the user can chose from
    this makes it easier to just uncomment the right options
    instead of having to read the docs and sources.

--- config.sh
+++ config.sh
@@ -1,4 +1,23 @@
 # ARCH will be auto-detected as the host if not specified
 #ARCH=i486
+#ARCH=x86_64
+#ARCH=powerpc
+#ARCH=arm
+#ARCH=microblaze
+#ARCH=mips
+#ARCH=mipsel
+
 CC_BASE_PREFIX=/opt/cross
+
+# if you use arm, you may need more fine-tuning:
+# arm hardfloat v7
+#TRIPLE=arm-linux-musleabihf
+#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+#GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+
+#arm softfp
+#TRIPLE=arm-linux-musleabi
+#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
+#GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
+
 MAKEFLAGS=-j8
