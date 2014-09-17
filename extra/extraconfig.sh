e6d260c2c4e96aac0fb0309700aa35c5d3246a20
Gregor Richards <hg-yff@gregor.im>
2014-09-17 15:04:09 -0400

    Build armhf tarballs with armv5+vfp by default.

--- extra/extraconfig.sh
+++ extra/extraconfig.sh
@@ -1,6 +1,11 @@
 # This is a suggested extraconfig.sh for build-tarballs.sh
 
 case "$ARCH" in
+    arm*hf)
+        GCC_BOOTSTRAP_CONFFLAGS="$GCC_BOOTSTRAP_CONFFLAGS --with-arch=armv5t --with-fpu=vfp"
+        GCC_CONFFLAGS="$GCC_CONFFLAGS --with-arch=armv5t --with-fpu=vfp"
+        ;;
+        
     arm*)
         GCC_BOOTSTRAP_CONFFLAGS="$GCC_BOOTSTRAP_CONFFLAGS --with-arch=armv4t"
         GCC_CONFFLAGS="$GCC_CONFFLAGS --with-arch=armv4t"
